cd67f96153264950954e000ba7f0267f29c38b58
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-01 22:04:02 +0200

    coffee machine

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from menu import Menu
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+should_continue = True
+menu = Menu()
+
+while should_continue:
+    print('What would you like? (espresso/latte/cappuccino/):')
+    user_input = input().lower()
+
+    if user_input == 'off':
+        should_continue = False
+    elif user_input == 'report':
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(user_input)
+        if coffee_maker.is_resource_sufficient(drink):
+            if  money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
